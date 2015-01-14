bec44e0dbd167baf01d9546f2155509c5912c78d
Khosrow Afroozeh <parshua@gmail.com>
2015-01-14 12:17:23 +0100

    changed bcrypt to internal pkg

--- admin_command.go
+++ admin_command.go
@@ -1,7 +1,7 @@
 // Copyright 2013-2014 Aerospike, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use acmd file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
@@ -17,9 +17,9 @@ package aerospike
 import (
 	"time"
 
+	"github.com/aerospike/aerospike-client-go/pkg/bcrypt"
 	. "github.com/aerospike/aerospike-client-go/types"
 	Buffer "github.com/aerospike/aerospike-client-go/utils/buffer"
-	"github.com/jameskeane/bcrypt"
 )
 
 const (
@@ -66,94 +66,94 @@ func newAdminCommand() *AdminCommand {
 	}
 }
 
-func (this *AdminCommand) authenticate(conn *Connection, user string, password []byte) error {
+func (acmd *AdminCommand) authenticate(conn *Connection, user string, password []byte) error {
 
-	this.setAuthenticate(user, password)
-	if _, err := conn.Write(this.dataBuffer[:this.dataOffset]); err != nil {
+	acmd.setAuthenticate(user, password)
+	if _, err := conn.Write(acmd.dataBuffer[:acmd.dataOffset]); err != nil {
 		return err
 	}
 
-	if _, err := conn.Read(this.dataBuffer, _HEADER_SIZE); err != nil {
+	if _, err := conn.Read(acmd.dataBuffer, _HEADER_SIZE); err != nil {
 		return err
 	}
 
-	result := this.dataBuffer[_RESULT_CODE]
+	result := acmd.dataBuffer[_RESULT_CODE]
 	if result != 0 {
 		return NewAerospikeError(ResultCode(result), "Authentication failed")
 	}
 
-	bufPool.Put(this.dataBuffer)
+	bufPool.Put(acmd.dataBuffer)
 
 	return nil
 }
 
-func (this *AdminCommand) setAuthenticate(user string, password []byte) int {
-	this.writeHeader(_AUTHENTICATE, 2)
-	this.writeFieldStr(_USER, user)
-	this.writeFieldBytes(_CREDENTIAL, password)
-	this.writeSize()
+func (acmd *AdminCommand) setAuthenticate(user string, password []byte) int {
+	acmd.writeHeader(_AUTHENTICATE, 2)
+	acmd.writeFieldStr(_USER, user)
+	acmd.writeFieldBytes(_CREDENTIAL, password)
+	acmd.writeSize()
 
-	return this.dataOffset
+	return acmd.dataOffset
 }
 
-func (this *AdminCommand) createUser(cluster *Cluster, policy *AdminPolicy, user string, password []byte, roles []string) error {
-	this.writeHeader(_CREATE_USER, 3)
-	this.writeFieldStr(_USER, user)
-	this.writeFieldBytes(_PASSWORD, password)
-	this.writeRoles(roles)
-	return this.executeCommand(cluster, policy)
+func (acmd *AdminCommand) createUser(cluster *Cluster, policy *AdminPolicy, user string, password []byte, roles []string) error {
+	acmd.writeHeader(_CREATE_USER, 3)
+	acmd.writeFieldStr(_USER, user)
+	acmd.writeFieldBytes(_PASSWORD, password)
+	acmd.writeRoles(roles)
+	return acmd.executeCommand(cluster, policy)
 }
 
-func (this *AdminCommand) dropUser(cluster *Cluster, policy *AdminPolicy, user string) error {
-	this.writeHeader(_DROP_USER, 1)
-	this.writeFieldStr(_USER, user)
-	return this.executeCommand(cluster, policy)
+func (acmd *AdminCommand) dropUser(cluster *Cluster, policy *AdminPolicy, user string) error {
+	acmd.writeHeader(_DROP_USER, 1)
+	acmd.writeFieldStr(_USER, user)
+	return acmd.executeCommand(cluster, policy)
 }
 
-func (this *AdminCommand) setPassword(cluster *Cluster, policy *AdminPolicy, user string, password []byte) error {
-	this.writeHeader(_SET_PASSWORD, 2)
-	this.writeFieldStr(_USER, user)
-	this.writeFieldBytes(_PASSWORD, password)
-	return this.executeCommand(cluster, policy)
+func (acmd *AdminCommand) setPassword(cluster *Cluster, policy *AdminPolicy, user string, password []byte) error {
+	acmd.writeHeader(_SET_PASSWORD, 2)
+	acmd.writeFieldStr(_USER, user)
+	acmd.writeFieldBytes(_PASSWORD, password)
+	return acmd.executeCommand(cluster, policy)
 }
 
-func (this *AdminCommand) changePassword(cluster *Cluster, policy *AdminPolicy, user string, password []byte) error {
-	this.writeHeader(_CHANGE_PASSWORD, 3)
-	this.writeFieldStr(_USER, user)
-	this.writeFieldBytes(_OLD_PASSWORD, cluster.password)
-	this.writeFieldBytes(_PASSWORD, password)
-	return this.executeCommand(cluster, policy)
+func (acmd *AdminCommand) changePassword(cluster *Cluster, policy *AdminPolicy, user string, password []byte) error {
+	acmd.writeHeader(_CHANGE_PASSWORD, 3)
+	acmd.writeFieldStr(_USER, user)
+	acmd.writeFieldBytes(_OLD_PASSWORD, cluster.password)
+	acmd.writeFieldBytes(_PASSWORD, password)
+	return acmd.executeCommand(cluster, policy)
 }
 
-func (this *AdminCommand) grantRoles(cluster *Cluster, policy *AdminPolicy, user string, roles []string) error {
-	this.writeHeader(_GRANT_ROLES, 2)
-	this.writeFieldStr(_USER, user)
-	this.writeRoles(roles)
-	return this.executeCommand(cluster, policy)
+func (acmd *AdminCommand) grantRoles(cluster *Cluster, policy *AdminPolicy, user string, roles []string) error {
+	acmd.writeHeader(_GRANT_ROLES, 2)
+	acmd.writeFieldStr(_USER, user)
+	acmd.writeRoles(roles)
+	return acmd.executeCommand(cluster, policy)
 }
 
-func (this *AdminCommand) revokeRoles(cluster *Cluster, policy *AdminPolicy, user string, roles []string) error {
-	this.writeHeader(_REVOKE_ROLES, 2)
-	this.writeFieldStr(_USER, user)
-	this.writeRoles(roles)
-	return this.executeCommand(cluster, policy)
+func (acmd *AdminCommand) revokeRoles(cluster *Cluster, policy *AdminPolicy, user string, roles []string) error {
+	acmd.writeHeader(_REVOKE_ROLES, 2)
+	acmd.writeFieldStr(_USER, user)
+	acmd.writeRoles(roles)
+	return acmd.executeCommand(cluster, policy)
 }
 
-func (this *AdminCommand) replaceRoles(cluster *Cluster, policy *AdminPolicy, user string, roles []string) error {
-	this.writeHeader(_REPLACE_ROLES, 2)
-	this.writeFieldStr(_USER, user)
-	this.writeRoles(roles)
-	return this.executeCommand(cluster, policy)
+func (acmd *AdminCommand) replaceRoles(cluster *Cluster, policy *AdminPolicy, user string, roles []string) error {
+	acmd.writeHeader(_REPLACE_ROLES, 2)
+	acmd.writeFieldStr(_USER, user)
+	acmd.writeRoles(roles)
+	return acmd.executeCommand(cluster, policy)
 }
 
-func (this *AdminCommand) queryUser(cluster *Cluster, policy *AdminPolicy, user string) (*UserRoles, error) {
+func (acmd *AdminCommand) queryUser(cluster *Cluster, policy *AdminPolicy, user string) (*UserRoles, error) {
 	// TODO: Remove the workaround in the future
 	time.Sleep(time.Millisecond * 10)
-	defer bufPool.Put(this.dataBuffer)
+	defer bufPool.Put(acmd.dataBuffer)
 
-	this.writeHeader(_QUERY_USERS, 1)
-	this.writeFieldStr(_USER, user)
-	list, err := this.readUsers(cluster, policy)
+	acmd.writeHeader(_QUERY_USERS, 1)
+	acmd.writeFieldStr(_USER, user)
+	list, err := acmd.readUsers(cluster, policy)
 	if err != nil {
 		return nil, err
 	}
@@ -165,77 +165,77 @@ func (this *AdminCommand) queryUser(cluster *Cluster, policy *AdminPolicy, user 
 	return nil, nil
 }
 
-func (this *AdminCommand) queryUsers(cluster *Cluster, policy *AdminPolicy) ([]*UserRoles, error) {
+func (acmd *AdminCommand) queryUsers(cluster *Cluster, policy *AdminPolicy) ([]*UserRoles, error) {
 	// TODO: Remove the workaround in the future
 	time.Sleep(time.Millisecond * 10)
-	defer bufPool.Put(this.dataBuffer)
+	defer bufPool.Put(acmd.dataBuffer)
 
-	this.writeHeader(_QUERY_USERS, 0)
-	list, err := this.readUsers(cluster, policy)
+	acmd.writeHeader(_QUERY_USERS, 0)
+	list, err := acmd.readUsers(cluster, policy)
 	if err != nil {
 		return nil, err
 	}
 	return list, nil
 }
 
-func (this *AdminCommand) writeRoles(roles []string) {
-	offset := this.dataOffset + int(_FIELD_HEADER_SIZE)
-	this.dataBuffer[offset] = byte(len(roles))
+func (acmd *AdminCommand) writeRoles(roles []string) {
+	offset := acmd.dataOffset + int(_FIELD_HEADER_SIZE)
+	acmd.dataBuffer[offset] = byte(len(roles))
 	offset++
 
 	for _, role := range roles {
-		len := copy(this.dataBuffer[offset+1:], role)
-		this.dataBuffer[offset] = byte(len)
+		len := copy(acmd.dataBuffer[offset+1:], role)
+		acmd.dataBuffer[offset] = byte(len)
 		offset += len + 1
 	}
 
-	size := offset - this.dataOffset - int(_FIELD_HEADER_SIZE)
-	this.writeFieldHeader(_ROLES, size)
-	this.dataOffset = offset
+	size := offset - acmd.dataOffset - int(_FIELD_HEADER_SIZE)
+	acmd.writeFieldHeader(_ROLES, size)
+	acmd.dataOffset = offset
 }
 
-func (this *AdminCommand) writeSize() {
+func (acmd *AdminCommand) writeSize() {
 	// Write total size of message which is the current offset.
-	var size = int64(this.dataOffset-8) | (_MSG_VERSION << 56) | (_MSG_TYPE << 48)
-	Buffer.Int64ToBytes(size, this.dataBuffer, 0)
+	var size = int64(acmd.dataOffset-8) | (_MSG_VERSION << 56) | (_MSG_TYPE << 48)
+	Buffer.Int64ToBytes(size, acmd.dataBuffer, 0)
 }
 
-func (this *AdminCommand) writeHeader(command byte, fieldCount int) {
+func (acmd *AdminCommand) writeHeader(command byte, fieldCount int) {
 	// Authenticate header is almost all zeros
-	for i := this.dataOffset; i < this.dataOffset+16; i++ {
-		this.dataBuffer[i] = 0
+	for i := acmd.dataOffset; i < acmd.dataOffset+16; i++ {
+		acmd.dataBuffer[i] = 0
 	}
-	this.dataBuffer[this.dataOffset+2] = command
-	this.dataBuffer[this.dataOffset+3] = byte(fieldCount)
-	this.dataOffset += 16
+	acmd.dataBuffer[acmd.dataOffset+2] = command
+	acmd.dataBuffer[acmd.dataOffset+3] = byte(fieldCount)
+	acmd.dataOffset += 16
 }
 
-func (this *AdminCommand) writeFieldStr(id byte, str string) {
-	len := copy(this.dataBuffer[this.dataOffset+int(_FIELD_HEADER_SIZE):], str)
-	this.writeFieldHeader(id, len)
-	this.dataOffset += len
+func (acmd *AdminCommand) writeFieldStr(id byte, str string) {
+	len := copy(acmd.dataBuffer[acmd.dataOffset+int(_FIELD_HEADER_SIZE):], str)
+	acmd.writeFieldHeader(id, len)
+	acmd.dataOffset += len
 }
 
-func (this *AdminCommand) writeFieldBytes(id byte, bytes []byte) {
-	copy(this.dataBuffer[this.dataOffset+int(_FIELD_HEADER_SIZE):], bytes)
-	this.writeFieldHeader(id, len(bytes))
-	this.dataOffset += len(bytes)
+func (acmd *AdminCommand) writeFieldBytes(id byte, bytes []byte) {
+	copy(acmd.dataBuffer[acmd.dataOffset+int(_FIELD_HEADER_SIZE):], bytes)
+	acmd.writeFieldHeader(id, len(bytes))
+	acmd.dataOffset += len(bytes)
 }
 
-func (this *AdminCommand) writeFieldHeader(id byte, size int) {
-	Buffer.Int32ToBytes(int32(size+1), this.dataBuffer, this.dataOffset)
-	this.dataOffset += 4
-	this.dataBuffer[this.dataOffset] = id
-	this.dataOffset++
+func (acmd *AdminCommand) writeFieldHeader(id byte, size int) {
+	Buffer.Int32ToBytes(int32(size+1), acmd.dataBuffer, acmd.dataOffset)
+	acmd.dataOffset += 4
+	acmd.dataBuffer[acmd.dataOffset] = id
+	acmd.dataOffset++
 }
 
-func (this *AdminCommand) executeCommand(cluster *Cluster, policy *AdminPolicy) error {
+func (acmd *AdminCommand) executeCommand(cluster *Cluster, policy *AdminPolicy) error {
 	// TODO: Remove the workaround in the future
 	defer time.Sleep(time.Millisecond * 10)
 
-	defer bufPool.Put(this.dataBuffer)
+	defer bufPool.Put(acmd.dataBuffer)
 
-	this.writeSize()
+	acmd.writeSize()
 	node, err := cluster.GetRandomNode()
 	if err != nil {
 		return nil
@@ -250,19 +250,19 @@ func (this *AdminCommand) executeCommand(cluster *Cluster, policy *AdminPolicy) 
 		return err
 	}
 
-	if _, err := conn.Write(this.dataBuffer[:this.dataOffset]); err != nil {
+	if _, err := conn.Write(acmd.dataBuffer[:acmd.dataOffset]); err != nil {
 		conn.Close()
 		return err
 	}
 
-	if _, err := conn.Read(this.dataBuffer, _HEADER_SIZE); err != nil {
+	if _, err := conn.Read(acmd.dataBuffer, _HEADER_SIZE); err != nil {
 		conn.Close()
 		return err
 	}
 
 	node.PutConnection(conn)
 
-	result := this.dataBuffer[_RESULT_CODE]
+	result := acmd.dataBuffer[_RESULT_CODE]
 	if result != 0 {
 		return NewAerospikeError(ResultCode(result))
 	}
@@ -270,8 +270,8 @@ func (this *AdminCommand) executeCommand(cluster *Cluster, policy *AdminPolicy) 
 	return nil
 }
 
-func (this *AdminCommand) readUsers(cluster *Cluster, policy *AdminPolicy) ([]*UserRoles, error) {
-	this.writeSize()
+func (acmd *AdminCommand) readUsers(cluster *Cluster, policy *AdminPolicy) ([]*UserRoles, error) {
+	acmd.writeSize()
 	node, err := cluster.GetRandomNode()
 	if err != nil {
 		return nil, err
@@ -286,12 +286,12 @@ func (this *AdminCommand) readUsers(cluster *Cluster, policy *AdminPolicy) ([]*U
 		return nil, err
 	}
 
-	if _, err := conn.Write(this.dataBuffer[:this.dataOffset]); err != nil {
+	if _, err := conn.Write(acmd.dataBuffer[:acmd.dataOffset]); err != nil {
 		conn.Close()
 		return nil, err
 	}
 
-	status, list, err := this.readUserBlocks(conn)
+	status, list, err := acmd.readUserBlocks(conn)
 	if err != nil {
 		return nil, err
 	}
@@ -303,26 +303,26 @@ func (this *AdminCommand) readUsers(cluster *Cluster, policy *AdminPolicy) ([]*U
 	return list, nil
 }
 
-func (this *AdminCommand) readUserBlocks(conn *Connection) (status int, rlist []*UserRoles, err error) {
+func (acmd *AdminCommand) readUserBlocks(conn *Connection) (status int, rlist []*UserRoles, err error) {
 
 	var list []*UserRoles
 
 	for status == 0 {
-		if _, err = conn.Read(this.dataBuffer, 8); err != nil {
+		if _, err = conn.Read(acmd.dataBuffer, 8); err != nil {
 			return -1, nil, err
 		}
 
-		size := Buffer.BytesToInt64(this.dataBuffer, 0)
+		size := Buffer.BytesToInt64(acmd.dataBuffer, 0)
 		receiveSize := (size & 0xFFFFFFFFFFFF)
 
 		if receiveSize > 0 {
-			if receiveSize > int64(len(this.dataBuffer)) {
-				this.dataBuffer = make([]byte, receiveSize)
+			if receiveSize > int64(len(acmd.dataBuffer)) {
+				acmd.dataBuffer = make([]byte, receiveSize)
 			}
-			if _, err = conn.Read(this.dataBuffer, int(receiveSize)); err != nil {
+			if _, err = conn.Read(acmd.dataBuffer, int(receiveSize)); err != nil {
 				return -1, nil, err
 			}
-			status, list, err = this.parseUsers(int(receiveSize))
+			status, list, err = acmd.parseUsers(int(receiveSize))
 			if err != nil {
 				return -1, nil, err
 			}
@@ -334,12 +334,12 @@ func (this *AdminCommand) readUserBlocks(conn *Connection) (status int, rlist []
 	return status, rlist, nil
 }
 
-func (this *AdminCommand) parseUsers(receiveSize int) (int, []*UserRoles, error) {
-	this.dataOffset = 0
+func (acmd *AdminCommand) parseUsers(receiveSize int) (int, []*UserRoles, error) {
+	acmd.dataOffset = 0
 	list := make([]*UserRoles, 0, 100)
 
-	for this.dataOffset < receiveSize {
-		resultCode := int(this.dataBuffer[this.dataOffset+1])
+	for acmd.dataOffset < receiveSize {
+		resultCode := int(acmd.dataBuffer[acmd.dataOffset+1])
 
 		if resultCode != 0 {
 			if resultCode == _QUERY_END {
@@ -349,23 +349,23 @@ func (this *AdminCommand) parseUsers(receiveSize int) (int, []*UserRoles, error)
 		}
 
 		userRoles := &UserRoles{}
-		fieldCount := int(this.dataBuffer[this.dataOffset+3])
-		this.dataOffset += _HEADER_REMAINING
+		fieldCount := int(acmd.dataBuffer[acmd.dataOffset+3])
+		acmd.dataOffset += _HEADER_REMAINING
 
 		for i := 0; i < fieldCount; i++ {
-			len := int(Buffer.BytesToInt32(this.dataBuffer, this.dataOffset))
-			this.dataOffset += 4
-			id := this.dataBuffer[this.dataOffset]
-			this.dataOffset++
+			len := int(Buffer.BytesToInt32(acmd.dataBuffer, acmd.dataOffset))
+			acmd.dataOffset += 4
+			id := acmd.dataBuffer[acmd.dataOffset]
+			acmd.dataOffset++
 			len--
 
 			if id == _USER {
-				userRoles.User = string(this.dataBuffer[this.dataOffset : this.dataOffset+len])
-				this.dataOffset += len
+				userRoles.User = string(acmd.dataBuffer[acmd.dataOffset : acmd.dataOffset+len])
+				acmd.dataOffset += len
 			} else if id == _ROLES {
-				this.parseRoles(userRoles)
+				acmd.parseRoles(userRoles)
 			} else {
-				this.dataOffset += len
+				acmd.dataOffset += len
 			}
 		}
 
@@ -382,16 +382,16 @@ func (this *AdminCommand) parseUsers(receiveSize int) (int, []*UserRoles, error)
 	return 0, list, nil
 }
 
-func (this *AdminCommand) parseRoles(userRoles *UserRoles) {
-	size := int(this.dataBuffer[this.dataOffset])
-	this.dataOffset++
+func (acmd *AdminCommand) parseRoles(userRoles *UserRoles) {
+	size := int(acmd.dataBuffer[acmd.dataOffset])
+	acmd.dataOffset++
 	userRoles.Roles = make([]string, 0, size)
 
 	for i := 0; i < size; i++ {
-		len := int(this.dataBuffer[this.dataOffset])
-		this.dataOffset++
-		role := string(this.dataBuffer[this.dataOffset : this.dataOffset+len])
-		this.dataOffset += len
+		len := int(acmd.dataBuffer[acmd.dataOffset])
+		acmd.dataOffset++
+		role := string(acmd.dataBuffer[acmd.dataOffset : acmd.dataOffset+len])
+		acmd.dataOffset += len
 		userRoles.Roles = append(userRoles.Roles, role)
 	}
 }
