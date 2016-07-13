45863b7fd8640dc12f7fdd397104d97e1986f25a
Khosrow Afroozeh <parshua@gmail.com>
2016-07-13 12:14:55 +0200

    update proxy_check.sh to work with server v3.9.0 and later.

--- .travis/proxy_check.sh
+++ .travis/proxy_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-./asinfo -p 3000 | grep -q ";proxy_action=0;"
+./asinfo -p 3000 -v "namespace/test" | grep -q ";client_proxy_complete=0;"
 if [ $? -ne 0 ]
 then
 	exit 1
 fi
 
-./asinfo -p 3010 | grep -q ";proxy_action=0;"
+./asinfo -p 3010 -v "namespace/test" | grep -q ";client_proxy_complete=0;"
 if [ $? -ne 0 ]
 then
 	exit 1
