f2d77938d5b973d4367db64963d0952ad74c9828
Khosrow Afroozeh <parshua@gmail.com>
2017-04-24 23:18:36 +0200

    Add tests for Operation caching

--- cdt_list_test.go
+++ cdt_list_test.go
@@ -97,6 +97,17 @@ var _ = Describe("CDT List Test", func() {
 			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(listSize))
 		})
 
+		It("should Get the last element again", func() {
+			ops := []*as.Operation{as.ListGetOp(cdtBinName, -1)}
+			cdtListRes, err := client.Operate(wpolicy, key, ops...)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(listSize))
+
+			cdtListRes, err = client.Operate(wpolicy, key, ops...)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(listSize))
+		})
+
 		It("should Get the last 3 element", func() {
 			cdtListRes, err := client.Operate(wpolicy, key, as.ListGetRangeOp(cdtBinName, -3, 3))
 			Expect(err).ToNot(HaveOccurred())
@@ -113,6 +124,25 @@ var _ = Describe("CDT List Test", func() {
 			cdtListRes, err := client.Operate(wpolicy, key, as.ListAppendOp(cdtBinName, math.MaxInt64))
 			Expect(err).ToNot(HaveOccurred())
 			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(listSize + 1))
+
+			cdtListRes, err = client.Operate(wpolicy, key, as.ListGetOp(cdtBinName, listSize))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(math.MaxInt64))
+		})
+
+		It("should append the same cached element to the tail", func() {
+			ops := []*as.Operation{as.ListAppendOp(cdtBinName, math.MaxInt64)}
+			cdtListRes, err := client.Operate(wpolicy, key, ops...)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(listSize + 1))
+
+			cdtListRes, err = client.Operate(wpolicy, key, as.ListGetOp(cdtBinName, listSize))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(math.MaxInt64))
+
+			cdtListRes, err = client.Operate(wpolicy, key, ops...)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(cdtListRes.Bins[cdtBinName]).To(Equal(listSize + 2))
 
 			cdtListRes, err = client.Operate(wpolicy, key, as.ListGetOp(cdtBinName, listSize))
 			Expect(err).ToNot(HaveOccurred())
