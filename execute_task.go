b16ebd21ce8c6da43fd69cc5803066eedca51e34
Khosrow Afroozeh <parshua@gmail.com>
2022-06-21 21:39:32 +0200

    [CLIENT-1770] Assume background query is complete when the server 6.0+ returns 'not found' in 'query-show' info command

--- execute_task.go
+++ execute_task.go
@@ -27,6 +27,10 @@ type ExecuteTask struct {
 
 	taskID uint64
 	scan   bool
+
+	// The following map keeps an account of what nodes were ever observed with the job registered on them.
+	// If the job was ever observed, the task will return true for it is not found anymore (purged from task queue after completetion)
+	observed map[string]struct{}
 }
 
 // NewExecuteTask initializes task with fields needed to query server nodes.
@@ -35,6 +39,7 @@ func NewExecuteTask(cluster *Cluster, statement *Statement) *ExecuteTask {
 		baseTask: newTask(cluster),
 		taskID:   statement.TaskId,
 		scan:     statement.IsScan(),
+		observed: make(map[string]struct{}, len(cluster.GetNodes())),
 	}
 }
 
@@ -73,20 +78,30 @@ func (etsk *ExecuteTask) IsDone() (bool, Error) {
 		response := responseMap[command]
 
 		if strings.HasPrefix(response, "ERROR:2") {
-			if etsk.retries.Get() > 20 {
-				// Task not found. This could mean task already completed or
-				// task not started yet.  We are going to have to assume that
-				// the task already completed...
-				continue
+			// If the server node is v6+, it will immediately put the job on queue, so if it is not found,
+			// it means that it is completed.
+			if !node.SupportsPartitionQuery() {
+				// Task not found. On server prior to v6, this could mean task was already completed or not started yet.
+				// If the job was not observed before, its completetion is in doubt.
+				// Otherwise it means it was completed.
+				if _, existed := etsk.observed[node.GetName()]; !existed && etsk.retries.Get() < 20 {
+					// If the job was not found in some nodes, it may mean that the job was not started yet.
+					// So we will keep retrying.
+					return false, nil
+				}
 			}
-			// Task not found, and number of retries are not enough; assume task is not started yet.
-			return false, nil
+
+			// Assume the task was completed.
+			continue
 		}
 
 		if strings.HasPrefix(response, "ERROR:") {
 			// Mark done and quit immediately.
 			return false, newError(types.UDF_BAD_RESPONSE, response)
 		}
+
+		// mark the node as observed
+		etsk.observed[node.GetName()] = struct{}{}
 
 		find := "status="
 		index := strings.Index(response, find)
