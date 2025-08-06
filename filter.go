6b6f9d4e857d62d5c6ffc817db5d122066d1e195
Mirza Karacic <mkaracic@aerospike.com>
2025-08-06 12:48:00 -0700

    Added additional filter as request per CLIENT-3615

--- filter.go
+++ filter.go
@@ -39,6 +39,20 @@ func NewEqualFilter(binName string, value interface{}, ctx ...*CDTContext) *Filt
 	return newFilter(binName, "", ICT_DEFAULT, val.GetType(), val, val, ctx, nil)
 }
 
+// NewEqualWithExpressionFilter creates an equality filter for query with an expression.
+// Value can be an integer, string or a blob (byte array). Byte arrays are only supported on server v7+.
+func NewEqualWithExpressionFilter(expression *Expression, value interface{}) *Filter {
+	v := NewValue(value)
+	return newFilter("", "", ICT_DEFAULT, ParticleType.INTEGER, v, v, nil, expression)
+}
+
+// NewEqualWithIndexNameFilter creates an equality filter for query with an index name.
+// Value can be an integer, string or a blob (byte array). Byte arrays are only supported on server v7+.
+func NewEqualWithIndexNameFilter(indexName string, value interface{}) *Filter {
+	v := NewValue(value)
+	return newFilter("", indexName, ICT_DEFAULT, ParticleType.INTEGER, v, v, nil, nil)
+}
+
 // NewRangeFilter creates a range filter for query.
 // Range arguments must be int64 values.
 // String ranges are not supported.
@@ -47,11 +61,39 @@ func NewRangeFilter(binName string, begin int64, end int64, ctx ...*CDTContext) 
 	return newFilter(binName, "", ICT_DEFAULT, vBegin.GetType(), vBegin, vEnd, ctx, nil)
 }
 
+// NewRangeWithExpressionFilter creates a range filter for query with an expression.
+// Range arguments must be int64 values.
+// String ranges are not supported.
+func NewRangeWithExpressionFilter(expression *Expression, begin Value, end Value) *Filter {
+	return newFilter("", "", ICT_DEFAULT, ParticleType.INTEGER, begin, end, nil, expression)
+}
+
+// NewRangeWithIndexNameFilter creates a range filter for query with an index name.
+// Range arguments must be int64 values.
+// String ranges are not supported.
+func NewRangeWithIndexNameFilter(indexName string, begin Value, end Value) *Filter {
+	return newFilter("", indexName, ICT_DEFAULT, ParticleType.INTEGER, begin, end, nil, nil)
+}
+
 // NewContainsFilter creates a contains filter for query on collection index.
 // Value can be an integer, string or a blob (byte array). Byte arrays are only supported on server v7+.
 func NewContainsFilter(binName string, indexCollectionType IndexCollectionType, value interface{}, ctx ...*CDTContext) *Filter {
 	v := NewValue(value)
 	return newFilter(binName, "", indexCollectionType, v.GetType(), v, v, ctx, nil)
+}
+
+// NewContainsWithExpressionFilter creates a contains filter for query on collection index with an expression.
+// Value can be an integer, string or a blob (byte array). Byte arrays are only supported on server v7+.
+func NewContainsWithExpressionFilter(expression *Expression, indexCollectionType IndexCollectionType, value interface{}) *Filter {
+	v := NewValue(value)
+	return newFilter("", "", indexCollectionType, v.GetType(), v, v, nil, expression)
+}
+
+// NewContainsWithIndexNameFilter creates a contains filter for query on collection index with an index name.
+// Value can be an integer, string or a blob (byte array). Byte arrays are only supported on server v7+.
+func NewContainsWithIndexNameFilter(indexName string, indexCollectionType IndexCollectionType, value interface{}) *Filter {
+	v := NewValue(value)
+	return newFilter("", indexName, indexCollectionType, v.GetType(), v, v, nil, nil)
 }
 
 // NewContainsRangeFilter creates a contains filter for query on ranges of data in a collection index.
@@ -67,11 +109,39 @@ func NewGeoWithinRegionFilter(binName, region string, ctx ...*CDTContext) *Filte
 	return newFilter(binName, "", ICT_DEFAULT, ParticleType.GEOJSON, v, v, ctx, nil)
 }
 
+// NewGeoWithinRegionWithExpressionFilter creates a geospatial "within region" filter for query with an expression.
+// Argument must be a valid GeoJSON region.
+func NewGeoWithinRegionWithExpressionFilter(expression *Expression, region string) *Filter {
+	v := NewStringValue(region)
+	return newFilter("", "", ICT_DEFAULT, ParticleType.GEOJSON, v, v, nil, expression)
+}
+
+// NewGeoWithinRegionWithIndexNameFilter creates a geospatial "within region" filter for query with an index name.
+// Argument must be a valid GeoJSON region.
+func NewGeoWithinRegionWithIndexNameFilter(indexName string, region string) *Filter {
+	v := NewStringValue(region)
+	return newFilter("", indexName, ICT_DEFAULT, ParticleType.GEOJSON, v, v, nil, nil)
+}
+
 // NewGeoWithinRegionForCollectionFilter creates a geospatial "within region" filter for query on collection index.
 // Argument must be a valid GeoJSON region.
 func NewGeoWithinRegionForCollectionFilter(binName string, collectionType IndexCollectionType, region string, ctx ...*CDTContext) *Filter {
 	v := NewStringValue(region)
 	return newFilter(binName, "", collectionType, ParticleType.GEOJSON, v, v, ctx, nil)
+}
+
+// NewGeoWithinRegionForCollectionWithExpressionFilter creates a geospatial "within region" filter for query on collection index with an expression.
+// Argument must be a valid GeoJSON region.
+func NewGeoWithinRegionForCollectionWithExpressionFilter(expression *Expression, collectionType IndexCollectionType, region string) *Filter {
+	v := NewStringValue(region)
+	return newFilter("", "", collectionType, ParticleType.GEOJSON, v, v, nil, expression)
+}
+
+// NewGeoWithinRegionForCollectionWithIndexNameFilter creates a geospatial "within region" filter for query on collection index with an index name.
+// Argument must be a valid GeoJSON region.
+func NewGeoWithinRegionForCollectionWithIndexNameFilter(indexName string, collectionType IndexCollectionType, region string) *Filter {
+	v := NewStringValue(region)
+	return newFilter("", indexName, collectionType, ParticleType.GEOJSON, v, v, nil, nil)
 }
 
 // NewGeoRegionsContainingPointFilter creates a geospatial "containing point" filter for query.
@@ -81,11 +151,39 @@ func NewGeoRegionsContainingPointFilter(binName, point string, ctx ...*CDTContex
 	return newFilter(binName, "", ICT_DEFAULT, ParticleType.GEOJSON, v, v, ctx, nil)
 }
 
+// NewGeoRegionsContainingPointWithExpressionFilter creates a geospatial "containing point" filter for query with an expression.
+// Argument must be a valid GeoJSON point.
+func NewGeoRegionsContainingPointWithExpressionFilter(expression *Expression, point string) *Filter {
+	v := NewStringValue(point)
+	return newFilter("", "", ICT_DEFAULT, ParticleType.GEOJSON, v, v, nil, expression)
+}
+
+// NewGeoRegionsContainingPointWithIndexNameFilter creates a geospatial "containing point" filter for query with an index name.
+// Argument must be a valid GeoJSON point.
+func NewGeoRegionsContainingPointWithIndexNameFilter(indexName string, point string) *Filter {
+	v := NewStringValue(point)
+	return newFilter("", indexName, ICT_DEFAULT, ParticleType.GEOJSON, v, v, nil, nil)
+}
+
 // NewGeoRegionsContainingPointForCollectionFilter creates a geospatial "containing point" filter for query on collection index.
 // Argument must be a valid GeoJSON point.
 func NewGeoRegionsContainingPointForCollectionFilter(binName string, collectionType IndexCollectionType, point string, ctx ...*CDTContext) *Filter {
 	v := NewStringValue(point)
 	return newFilter(binName, "", collectionType, ParticleType.GEOJSON, v, v, ctx, nil)
+}
+
+// NewGeoRegionsContainingPointForCollectionWithExpressionFilter creates a geospatial "containing point" filter for query on collection index with an expression.
+// Argument must be a valid GeoJSON point.
+func NewGeoRegionsContainingPointForCollectionWithExpressionFilter(expression *Expression, collectionType IndexCollectionType, point string) *Filter {
+	v := NewStringValue(point)
+	return newFilter("", "", collectionType, ParticleType.GEOJSON, v, v, nil, expression)
+}
+
+// NewGeoRegionsContainingPointForCollectionWithIndexNameFilter creates a geospatial "containing point" filter for query on collection index with an index name.
+// Argument must be a valid GeoJSON point.
+func NewGeoRegionsContainingPointForCollectionWithIndexNameFilter(indexName string, collectionType IndexCollectionType, point string) *Filter {
+	v := NewStringValue(point)
+	return newFilter("", indexName, collectionType, ParticleType.GEOJSON, v, v, nil, nil)
 }
 
 // NewGeoWithinRadiusFilter creates a geospatial "within radius" filter for query.
@@ -95,11 +193,39 @@ func NewGeoWithinRadiusFilter(binName string, lng, lat, radius float64, ctx ...*
 	return newFilter(binName, "", ICT_DEFAULT, ParticleType.GEOJSON, NewValue(rgnStr), NewValue(rgnStr), ctx, nil)
 }
 
+// NewGeoWithinRadiusWithExpressionFilter creates a geospatial "within radius" filter for query with an expression.
+// Arguments must be valid longitude/latitude/radius (meters) values.
+func NewGeoWithinRadiusWithExpressionFilter(expression *Expression, lng, lat, radius float64) *Filter {
+	rgnStr := fmt.Sprintf("{ \"type\": \"AeroCircle\", "+"\"coordinates\": [[%.8f, %.8f], %f] }", lng, lat, radius)
+	return newFilter("", "", ICT_DEFAULT, ParticleType.GEOJSON, NewValue(rgnStr), NewValue(rgnStr), nil, expression)
+}
+
+// NewGeoWithinRadiusWithIndexNameFilter creates a geospatial "within radius" filter for query with an index name.
+// Arguments must be valid longitude/latitude/radius (meters) values.
+func NewGeoWithinRadiusWithIndexNameFilter(indexName string, lng, lat, radius float64) *Filter {
+	rgnStr := fmt.Sprintf("{ \"type\": \"AeroCircle\", "+"\"coordinates\": [[%.8f, %.8f], %f] }", lng, lat, radius)
+	return newFilter("", indexName, ICT_DEFAULT, ParticleType.GEOJSON, NewValue(rgnStr), NewValue(rgnStr), nil, nil)
+}
+
 // NewGeoWithinRadiusForCollectionFilter creates a geospatial "within radius" filter for query on collection index.
 // Arguments must be valid longitude/latitude/radius (meters) values.
 func NewGeoWithinRadiusForCollectionFilter(binName string, collectionType IndexCollectionType, lng, lat, radius float64, ctx ...*CDTContext) *Filter {
 	rgnStr := fmt.Sprintf("{ \"type\": \"AeroCircle\", "+"\"coordinates\": [[%.8f, %.8f], %f] }", lng, lat, radius)
 	return newFilter(binName, "", collectionType, ParticleType.GEOJSON, NewValue(rgnStr), NewValue(rgnStr), ctx, nil)
+}
+
+// NewGeoWithinRadiusForCollectionWithExpressionFilter creates a geospatial "within radius" filter for query on collection index with an expression.
+// Arguments must be valid longitude/latitude/radius (meters) values.
+func NewGeoWithinRadiusForCollectionWithExpressionFilter(expression *Expression, collectionType IndexCollectionType, lng, lat, radius float64) *Filter {
+	rgnStr := fmt.Sprintf("{ \"type\": \"AeroCircle\", "+"\"coordinates\": [[%.8f, %.8f], %f] }", lng, lat, radius)
+	return newFilter("", "", collectionType, ParticleType.GEOJSON, NewValue(rgnStr), NewValue(rgnStr), nil, expression)
+}
+
+// NewGeoWithinRadiusForCollectionWithIndexNameFilter creates a geospatial "within radius" filter for query on collection index with an index name.
+// Arguments must be valid longitude/latitude/radius (meters) values.
+func NewGeoWithinRadiusForCollectionWithIndexNameFilter(indexName string, collectionType IndexCollectionType, lng, lat, radius float64) *Filter {
+	rgnStr := fmt.Sprintf("{ \"type\": \"AeroCircle\", "+"\"coordinates\": [[%.8f, %.8f], %f] }", lng, lat, radius)
+	return newFilter("", indexName, collectionType, ParticleType.GEOJSON, NewValue(rgnStr), NewValue(rgnStr), nil, nil)
 }
 
 // Create a filter for query.
@@ -109,14 +235,18 @@ func NewFilter(name string, indexCollectionType IndexCollectionType, valuePartic
 	return newFilter(name, "", indexCollectionType, valueParticleType, begin, end, ctx, nil)
 }
 
-// NewRangeWithExpressionFilter creates a range filter for query with an expression.
-func NewRangeWithExpressionFilter(expression *Expression, begin Value, end Value) *Filter {
-	return newFilter("", "", ICT_DEFAULT, ParticleType.INTEGER, begin, end, nil, expression)
+// NewWithExpressionFilter creates a filter for query with an expression.
+// Range arguments must be longs or integers which can be cast to longs.
+// String ranges are not supported.
+func NewWithExpressionFilter(expression *Expression, indexCollectionType IndexCollectionType, valueParticleType int, begin Value, end Value) *Filter {
+	return newFilter("", "", indexCollectionType, valueParticleType, begin, end, nil, expression)
 }
 
-// NewRangeWithIndexNameFilter creates a range filter for query with an index name.
-func NewRangeWithIndexNameFilter(indexName string, begin Value, end Value) *Filter {
-	return newFilter("", indexName, ICT_DEFAULT, ParticleType.INTEGER, begin, end, nil, nil)
+// NewWithIndexNameFilter creates a filter for query with an index name.
+// Range arguments must be longs or integers which can be cast to longs.
+// String ranges are not supported.
+func NewWithIndexNameFilter(indexName string, indexCollectionType IndexCollectionType, valueParticleType int, begin Value, end Value) *Filter {
+	return newFilter("", indexName, indexCollectionType, valueParticleType, begin, end, nil, nil)
 }
 
 // newFilter creates a filter for query.
