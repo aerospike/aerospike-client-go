f0ce8c8f3da120717197b4e4cda2cf90a07ce5d8
Mirza Karacic <97757567+mirzakaracic@users.noreply.github.com>
2025-09-06 03:01:56 -0700

    CLIENT-3023 Allow repeat background query with the same statement (#509)

--- execute_task.go
+++ execute_task.go
@@ -36,10 +36,10 @@ type ExecuteTask struct {
 }
 
 // NewExecuteTask initializes task with fields needed to query server nodes.
-func NewExecuteTask(cluster *Cluster, statement *Statement) *ExecuteTask {
+func NewExecuteTask(cluster *Cluster, statement *Statement, taskId uint64) *ExecuteTask {
 	return &ExecuteTask{
 		baseTask: newTask(cluster),
-		taskID:   statement.TaskId,
+		taskID:   taskId,
 		scan:     statement.IsScan(),
 		observed: make(map[string]struct{}, len(cluster.GetNodes())),
 	}
