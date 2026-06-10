2960d39e175a1b157b2824951a8582f6e555cd2f
Mirza Karacic <mkaracic@aerospike.com>
2026-06-10 12:33:44 -0700

    Bumper server version for utf8 validation testing to 8.1.3

--- string_invalid_utf8_test.go
+++ string_invalid_utf8_test.go
@@ -69,7 +69,7 @@ var _ = gg.Describe("String Invalid UTF-8 Tests", func() {
 	}
 
 	gg.BeforeEach(func() {
-		requiredVersion, err := version.Parse("8.1.2")
+		requiredVersion, err := version.Parse("8.1.3")
 		if err != nil {
 			gg.Fail("Failed to parse server required version")
 		}
